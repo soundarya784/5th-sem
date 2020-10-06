647fb24361880301eb569ad636185ba3f82bbd2b
soundarya784 <soundarya2000.sl@gmail.com>
2020-10-06 20:25:23 +0530

    Add files via upload

---
+++ ADS/skiplist/skiplist.py
@@ -0,0 +1,65 @@
+import random
+class Node:
+    def __init__(self,key,level):
+        self.key = key
+        self.forward = [None]*(level+1)
+
+
+class skipList:
+    def __init__(self, max_lvl):
+        self.max_lvl = max_lvl
+        self.level = 0
+        self.head = self.create_node(self.max_lvl, -1)
+
+    def create_node(self, key, lvl):
+        new_node = Node(lvl, key)
+        return new_node
+
+    def insert_node(self, key):
+        update = [None] * (self.max_lvl+1)
+        cur = self.head
+
+        for i in range(self.level, -1, -1):
+            while cur.forward[i] and cur.forward[i].key < key:
+                cur = cur.forward[i]
+            update[i] = cur
+
+        ran_level = random.randint(0, self.max_lvl-1)
+        if ran_level > self.level:
+            self.level = ran_level
+
+        new_node = self.create_node(ran_level, key)
+
+        for i in range(ran_level + 1):
+            if update[i] == None:
+                new_node.forward[i] = None
+            else:
+                new_node.forward[i] = update[i].forward[i]
+            if update[i] == None:
+                update[i] = None
+            else:
+                update[i].forward[i] = new_node
+
+            print(f"inserted a node with {key}")
+
+    def displayList(self):
+        temp = self.head
+        for i in range(self.level + 1):
+            cur = self.head.forward[i]
+            print(f"level {i}")
+            while cur != None:
+                print(cur.key)
+                cur = cur.forward[i]
+
+
+
+
+
+li = skipList(2)
+li.insert_node(3)
+li.insert_node(4)
+li.insert_node(10)
+li.insert_node(2)
+li.displayList()
+
+
